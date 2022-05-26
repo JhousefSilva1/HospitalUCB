52c14c894bae77af1c3d886a762ad746b8939692
JosePozoSilva <jhosede23@gmail.com>
2022-05-26 10:22:16 -0400

    components

---
+++ HopitalUCB/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FootComponent } from './components/foot/foot.component';
+import { NavComponent } from './components/nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+    HomeComponent,
+     FootComponent,
+     NavComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
